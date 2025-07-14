bd3d88ce41087f28724a26dc465cc1240c45f37e
marsitos <marvin092004@gmail.com>
2025-07-14 18:16:42 -0500

    Delete repo

--- src/main/java/com/metrovia/repository/UnidadRepository.java
+++
@@ -1,8 +0,0 @@
-package com.metrovia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.metrovia.model.Unidad;
-
-public interface UnidadRepository extends JpaRepository<Unidad, String> {
-}
